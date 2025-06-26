e341c8dd3e906245008195050bec68b0d0bac7ce
hernik-dev <niklas.herpertz@mailbox.org>
2025-06-26 09:57:22 +0200

    Update buch_ranking_app.py Debug2

--- buch_ranking_app.py
+++ buch_ranking_app.py
@@ -29,7 +29,14 @@ st.title("📚 Effizientes Buch-Ranking per interaktivem Sortieren")
 # -------------------------------------------
 
 def merge(left, right):
-    return {"type": "merge", "left": left, "right": right, "result": [], "i": 0, "j": 0}
+    return {
+        "type": "merge",
+        "left": left,
+        "right": right,
+        "result": [],
+        "i": 0,
+        "j": 0
+    }
 
 def init_sort(lst):
     stack = []
@@ -40,48 +47,4 @@ def init_sort(lst):
         a = stack.pop()
         b = stack.pop()
         ops.append(merge(b, a))  # Reihenfolge beachten
-    return ops[::-1]  # umgekehrte Reihenfolge, damit man mit pop() von vorne beginnt
-
-# ----------------------------
-# Initialisierung in Session
-# ----------------------------
-
-if "ops" not in st.session_state:
-    st.session_state.ops = init_sort(books)
-    st.session_state.current = None
-    st.session_state.finished = False
-    st.session_state.history = []
-    st.session_state.result = None
-    st.session_state.decision_count = 0
-
-# --------------------------------------
-# Weiterverarbeitung nach Benutzerwahl
-# --------------------------------------
-
-def process_choice(choice):
-    op = st.session_state.current
-    if choice == "left":
-        op["result"].append(op["left"][op["i"]])
-        op["i"] += 1
-    elif choice == "right":
-        op["result"].append(op["right"][op["j"]])
-        op["j"] += 1
-
-    # Wenn ein Teil aufgebraucht ist, Rest anhängen
-    if op["i"] >= len(op["left"]):
-        op["result"].extend(op["right"][op["j"]:])
-        st.session_state.ops.append(op["result"])
-        st.session_state.current = None
-    elif op["j"] >= len(op["right"]):
-        op["result"].extend(op["left"][op["i"]:])
-        st.session_state.ops.append(op["result"])
-        st.session_state.current = None
-
-    st.session_state.decision_count += 1
-
-# --------------------------------------
-# Hauptlogik
-# --------------------------------------
-
-# Wenn gerade kein aktiver Vergleich läuft
-if not st.session_state.current and len(st.session_state.ops) > 1:
+    return ops[::-1]  # umgekehrte Reihenf
